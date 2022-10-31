921dc511ea058dc04ac19fbb8ccebfe624a9a35d
Vadim Shulgin <vadimshulgin@Vadims-Mac-mini.local>
2022-10-31 16:20:06 +0600

    Initial commit

---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'empty_grocery_screen.dart';
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add a scaffold widget
+    return const EmptyGroceryScreen();
+  }
+// TODO: Add buildGroceryScreen
+}
